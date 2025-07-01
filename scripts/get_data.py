468b488babfea9e5fe89b25945927f2704e17410
浑浑噩噩 <437303994@qq.com>
2025-07-01 20:53:52 +0800

    chore: Update script header and remove commented-out code in get_data.py

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -7,3 +7,9 @@ from qlib.tests.data import GetData
 
 if __name__ == "__main__":
     fire.Fire(GetData)
+    # GetData().qlib_data(
+    #     interval='1d',
+    #     region="cn",
+    #     delete_old=True,
+    #     exists_skip=False,
+    # )
