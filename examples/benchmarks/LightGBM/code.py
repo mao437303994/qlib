1f18dabf96bfb2269735d2fd22ce7d3e79a39da3
浑浑噩噩 <437303994@qq.com>
2025-07-02 15:34:40 +0800

    feat: Implement futures_fees_info function to fetch and process futures fees data

--- examples/benchmarks/LightGBM/code.py
+++ examples/benchmarks/LightGBM/code.py
@@ -1,7 +1,29 @@
-
 from os import path
+from qlib.contrib.strategy.signal_strategy import TopkDropoutStrategy
 from qlib.workflow.cli import workflow
+from qlib.workflow import R
+import qlib
+from qlib.workflow.record_temp import PortAnaRecord
+from qlib.data import D
 
 if __name__ == "__main__":
-    path = path.join(path.dirname(__file__), "workflow_config_lightgbm_Alpha158.yaml")
-    workflow(config_path=path)
+    # path = path.join(path.dirname(__file__), "workflow_config_lightgbm_Alpha158.yaml")
+    # workflow(config_path=path)
+
+    # 761191810450610219
+    # b9a09d2216804d8c9f7085e840a82a6c
+
+    qlib.init(provider_uri="~/.qlib/qlib_data/cn_data", region="cn")
+    # df = D.features(
+    #     instruments=["SH601008"],
+    #     fields=["$close", "$open", "$high", "$low", "$volume", "$change", "$factor"],
+    #     start_time="2020-01-01",
+    #     end_time="2020-01-05",
+    # )
+    # print(df)
+    recorder = R.get_recorder(
+        recorder_id="b9a09d2216804d8c9f7085e840a82a6c",
+        experiment_id="761191810450610219",
+    )
+    pr = PortAnaRecord(recorder=recorder)
+    pr.generate()
